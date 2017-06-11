56cdb1f036b7d342e58ca3fe4d0dad62131c4bf4
Louis <timeandust@google.com>
2017-06-11 17:03:23 +0800

    Add Segment and Point class

---
+++ CH 2/Segment.py
@@ -0,0 +1,22 @@
+class Point:
+    def __init__(self, x, y):
+        self.coordinate = (x, y)
+
+    def x(self):
+        return self.coordinate[0]
+
+    def y(self):
+        return self.coordinate[0]
+
+def Segment:
+    def __init__(self, start, end):
+        self.start_point = start
+        self.end_point = end
+
+    def start(self):
+        return self.start_point
+
+    def end(self):
+        return self.end_point
+    
+              
