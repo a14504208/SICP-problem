cc32da8306a6a04ccff9f24d1f0835ce89952ee3
Louis <timeandust@google.com>
2017-06-11 17:10:24 +0800

    Add string representation and test code for classes

--- CH 2/Segment.py
+++ CH 2/Segment.py
@@ -6,9 +6,15 @@ class Point:
         return self.coordinate[0]
 
     def y(self):
-        return self.coordinate[0]
+        return self.coordinate[1]
 
-def Segment:
+    def __str__(self):
+        return '({0}, {1})'.format(self.x(), self.y())
+
+    def __repr__(self):
+        return 'Point({0}, {1})'.format(self.x(), self.y())
+
+class Segment:
     def __init__(self, start, end):
         self.start_point = start
         self.end_point = end
@@ -18,5 +24,16 @@ def Segment:
 
     def end(self):
         return self.end_point
-    
+
+    def __str__(self):
+        return '{0} -> {1}'.format(self.start(), self.end())
+
+    def __repr__(self):
+        return 'Segment({0}, {1})'.format(repr(self.start()), repr(self.end()))
               
+if __name__ == '__main__':
+    a = Point(3, 5)
+    b = Point(4, 6)
+    print(a, b, sep=', ')
+    line = Segment(a, b)
+    print(line)
